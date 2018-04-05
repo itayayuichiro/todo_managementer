e66440719e6a847c7913f501670d01ee17cca0e0
Yuichiro Itaya <mr.itaya.magic@gmail.com>
2018-04-05 09:11:49 +0900

    fix: ファイル名変更

--- app/View/Helper/FormatHelper.php
+++ app/View/Helper/FormatHelper.php
@@ -3,6 +3,7 @@ class FormatHelper extends AppHelper {
   function format_date($date) {
   	$date = explode(" ", $date);
   	$str = explode("-", $date[0]);
+    
   	return $str[0]. '年' .$str[1]. '月'.$str[2]. '日';
   }
   function past_date($date){
